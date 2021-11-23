3520b9c9dba126821059b615bd4a7a67371b07e4
Michael Ozoudeh <jesmikky@gmail.com>
2021-11-23 10:29:26 +0100

    updates

--- morgans/resources/views/layouts/app.blade.php
+++ morgans/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('Morgans Consulting') }}</title>
+        <title>Morgans Consulting</title>
         <link rel="shortcut icon" href="{{asset('/assets/images/favicon.png')}}">
 
         <!-- Fonts -->
